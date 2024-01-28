07afd2e34a70672deb2cd02ef1f7efa60109d17a
Sunanda Carlström <carlstrom-3@hotmail.com>
2024-01-28 12:36:12 +0100

    Finjusterat grundlayout samt gjort klart AboutPage

--- src/components/Pages/About/AboutPage.jsx
+++ src/components/Pages/About/AboutPage.jsx
@@ -1,3 +1,5 @@
+import styles from "./AboutPage.module.css";
+import Image from "../../../../src/assets/images/sunanda_photoshop_icecream.jpg";
 import Gradient from "../../ui/Gradient/Gradient";
 import Navbar from "../../Shared/Navbar";
 import MainContent from "../../ui/MainContent/MainContent";
@@ -5,18 +7,32 @@ import MainContent from "../../ui/MainContent/MainContent";
 const About = () => {
     return (
         <>
-            <Gradient />
             <Navbar />
-            <MainContent>
-                <h1>[VEM ÄR JAG?]</h1>
-                <p>
-                    Hej! Jag heter Sunanda och har som dröm att bli Fullstack-utvecklare. Idag studerar jag en YH-utbildning inom Webbutveckling .NET. Som person är jag nyfiken, positiv, ambitiös och
-                    kreativ. Min nyfikenhet och min starka vilja att lära mig mer är de egenskaper som driver mig framåt. Jag har tidigare studerat en akademisk utbildning inom Medie- och
-                    kommunikationsvetenskap, samt en annan YH-utbildning inom Grafisk design och kommunikation. Jag tror att kombinationen av mina personliga egenskaper som tekniskt intresserad och
-                    väldigt social person kommer att vara till stor fördel inom yrket som utvecklare.
-                </p>
-                <p>Ser fram emot att få börja arbeta som utvecklare och bidra till tech-branschen!</p>
-            </MainContent>
+            <Gradient>
+                <MainContent>
+                    <h1 className={styles.title}>[ VEM ÄR JAG? ]</h1>
+                    <div className={styles.infoBox}>
+                        <img src={Image} alt="Sunanda Carlström" className={styles.avatar} />
+                        <p>
+                            Hej! Jag heter Sunanda och har som dröm att bli Fullstack-utvecklare.
+                            Idag studerar jag en YH-utbildning inom Webbutveckling .NET.
+                        </p>
+                        <p>
+                            Som person är jag nyfiken, positiv, ambitiös och kreativ. Min nyfikenhet
+                            och min starka vilja att lära mig mer är de egenskaper som driver mig
+                            framåt. Jag har tidigare studerat en akademisk utbildning inom Medie-
+                            och kommunikationsvetenskap, samt en annan YH-utbildning inom Grafisk
+                            design och kommunikation.
+                        </p>
+                        <p>
+                            Jag tror att kombinationen av mina personliga egenskaper som tekniskt
+                            intresserad och väldigt social person kommer att vara till stor fördel
+                            inom yrket som utvecklare.
+                        </p>
+                        <p>Ser fram emot att kunna bidra till tech-branschen som utvecklare!</p>
+                    </div>
+                </MainContent>
+            </Gradient>
         </>
     );
 };
